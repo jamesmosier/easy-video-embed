c1ee443f76140c01cd09c01ed25fe4ac5e46fc5b
James Mosier <jamesdmosier@gmail.com>
2015-05-02 20:06:53 -0400

    worked on gh pages build and homepage of docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,20 +8,6 @@ module.exports = function(grunt) {
     //Give the Grunt plugins their instructions!
     var taskConfig = {
 
-        //https://github.com/gruntjs/grunt-contrib-connect
-        // connect: {
-        //     server: {
-        //         options: {
-        //             port: 3000,
-        //             base: '<%= docs_dir %>',
-        //             // Change this to '0.0.0.0' to access the server from outside.
-        //             hostname: 'localhost',
-        //             livereload: true,
-        //             open: true
-        //         }
-        //     }
-        // },
-
         //combine files like this: 'dist/js/output.js': ['js/input.js', 'js/input2.js']
         uglify: {
             scriptz: {
@@ -92,7 +78,7 @@ module.exports = function(grunt) {
                 }
             },
             sass: {
-                files: '_sass/*.scss',
+                files: '<%= docs_dir %>/_sass/*.scss',
                 tasks: ['sass', 'cssmin'],
                 options: {
                     livereload: true
@@ -150,31 +136,18 @@ module.exports = function(grunt) {
             },
 
         },
-
-
-        /**
-         * The `index` task compiles the `index.html` file as a Grunt template. CSS
-         * and JS files co-exist here but they get split apart later.
-         */
-        // index: {
-
-        //   /**
-        //    * During development, we don't want to have wait for compilation,
-        //    * concatenation, minification, etc. So to avoid these steps, we simply
-        //    * add all script files directly to the `<head>` of `index.html`. The
-        //    * `src` property contains the list of included files.
-        //    */
-        //   build: {
-        //     dir: '<%= build_dir %>',
-        //     src: [
-        //       '<%= build_dir %>/js/*.js',
-        //       '<%= build_dir %>/css/*.css'
-        //     ]
-        //   }
-        // }
+        //run: grunt gh-pages
+        //grunt gh-pages --gh-pages-tag 'v3.2.1'
+        'gh-pages': {
+            options: {
+              base: '<%= docs_dir %>/_site',
+              message: 'pushing to ghPages from master'
+            },
+            src: ['**/*']
+          }
 
     };
-
+//, 'images/*', 'js/*', 'bower_packages/**/**/*', 'css/*', 'lib/*'
     grunt.initConfig(grunt.util._.extend(taskConfig, userConfig));
 
 
@@ -185,52 +158,6 @@ module.exports = function(grunt) {
     grunt.registerTask('default', ['build', 'connect', 'watch']);
 
 
-    /**
-     * A utility function to get all app JavaScript sources.
-     */
-    function filterForJS(files) {
-        return files.filter(function(file) {
-            return file.match(/\.js$/);
-        });
-    }
-
-    /**
-     * A utility function to get all app CSS sources.
-     */
-    function filterForCSS(files) {
-        return files.filter(function(file) {
-            return file.match(/\.css$/);
-            //return file.match(/^(?:(?!.min\.css$).)*\.css$/);
-        });
-    }
-
-    /**
-     * The index.html template includes the stylesheet and javascript sources
-     * based on dynamic names calculated in this Gruntfile. This task assembles
-     * the list into variables for the template to use and then runs the
-     * compilation.
-     */
-    grunt.registerMultiTask('index', 'Process index.html template', function() {
-        var dirRE = new RegExp('^(' + grunt.config('build_dir') + ')\/', 'g');
-        var jsFiles = filterForJS(this.filesSrc).map(function(file) {
-            return file.replace(dirRE, '');
-        });
-        var cssFiles = filterForCSS(this.filesSrc).map(function(file) {
-            return file.replace(dirRE, '');
-        });
-
-        grunt.file.copy('index.html', this.data.dir + '/index.html', {
-            process: function(contents, path) {
-                return grunt.template.process(contents, {
-                    data: {
-                        scripts: jsFiles,
-                        styles: cssFiles,
-                        version: grunt.config('pkg.version')
-                    }
-                });
-            }
-        });
-    });
 
 
 
